5dba82aa42daa09634451ac4c3bb36ad349a915b
Michele Silva <michele.silva@gmail.com>
2015-04-13 15:52:05 -0300

    Add setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='remd_temperature',
+    version='1.0',
+    py_modules=['remd_temperature'],
+    install_requires=[
+        'requests',
+        'beautifulsoup'
+    ]
+)
